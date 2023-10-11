430936437f1faac762937b0c7bb3e1a7da6cdf2b
ricof30 <ricofontecilla30@gmail.com>
2023-10-11 13:50:49 +0800

    added files

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+
+
+
+const routes = [
+// dito mo ilagay ung routes sa baba neto tapos pabura nong comment hehehe
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
