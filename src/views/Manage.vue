c17ba40d7624ac44292278ff3792e13f8a3713e5
ricof30 <97009967+ricof30@users.noreply.github.com>
2023-10-11 15:04:26 +0800

    added a file

---
+++ src/views/Manage.vue
@@ -0,0 +1,19 @@
+<template>
+  
+
+  <body>
+    <h1>  Manage</h1>
+	</body>
+
+    
+      </template>
+
+      <style>
+    
+      </style>
+
+   <script>
+export default{
+  name:'manage'
+}
+</script>
